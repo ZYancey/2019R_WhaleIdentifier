c01a87553184e7baa96a747a45ee3441eac34d4c
ZYancey <yanceyzack@gmail.com>
2023-11-03 17:10:13 -0600

    Fixes Feature Extraction to pull mean

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,12 +1,13 @@
 import os
 import librosa
+import numpy as np
 import pandas as pd
 
 
 # Function to extract MFCC features from an audio file
 def extract_mfcc_features(file_path, n_mfcc=10):
     audio_data, sampling_rate = librosa.load(file_path)
-    mfccs = librosa.feature.mfcc(y=audio_data, sr=sampling_rate, n_mfcc=n_mfcc)
+    mfccs = librosa.feature.mfcc(y=audio_data, sr=sampling_rate, n_mfcc=n_mfcc, n_mels=n_mfcc)
     return mfccs.T  # Transpose to have features in columns
 
 
@@ -23,13 +24,15 @@ def process_audio_files(directory, output_csv, n_mfcc=10):
                 print(f"{count}/{len(files)} | Extracting MFCC Data from: {file_path}")
                 subfolder_name = os.path.basename(root)
                 mfccs = extract_mfcc_features(file_path, n_mfcc)
+                mfccs_scaled_features = np.mean(mfccs.T, axis=0)
+                # print(mfccs_scaled_features)
                 data['species'].append(subfolder_name)
                 for i in range(n_mfcc):
-                    if i < len(mfccs):
-                        data[f'mfcc_{i}'].append(mfccs[i])
+                    if i < len(mfccs_scaled_features):
+                        data[f'mfcc_{i}'].append(mfccs_scaled_features[i])
                     else:
                         # Pad with zeros for missing features
-                        data[f'mfcc_{i}'].append([0] * n_mfcc)
+                        data[f'mfcc_{i}'].append(0)
 
     df = pd.DataFrame(data)
     df.to_csv(output_csv, index=False)
