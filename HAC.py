ebd5954f11bd140a1b62dda7b46b2ead42cdb7db
Eliza <eliza.kartchner@gmail.com>
2023-12-01 18:44:27 -0700

    add final results

---
+++ HAC.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering, KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import os
+import pickle
+
+def hac_test(file_name, output_folder="models"):
+    os.makedirs(output_folder, exist_ok=True)
+
+    df = pd.read_csv(file_name)
+
+    X = df.drop('species', axis=1)
+    y = df['species']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    hac = AgglomerativeClustering(n_clusters=3, linkage='complete')
+
+    hac.fit(X_train, y_train)
+
+    # Save the trained model using pickle
+    model_filename = os.path.join(output_folder, f"HAC_{os.path.splitext(os.path.basename(file_name))[0]}_model.pkl")
+    with open(model_filename, 'wb') as model_file:
+        pickle.dump(hac, model_file)
+
+    y_pred = hac.predict(X_test)
+
+    train_accuracy = hac.score(X_train, y_train)
+    test_accuracy = hac.score(X_test, y_test)
+
+    print(f"Train Accuracy: {train_accuracy}")
+    print(f"Test Accuracy: {test_accuracy}")
+    print(f"HAC Model saved to: {model_filename}")
+
+# Run HAC on all the files
+file_names = ["features_mfcc_only.csv",
+              "features_mfcc_only_noisegate.csv",
+              "features_fulldata_not_normalized.csv",
+              "features_fulldata_not_normalized_noisegate.csv",
+              "features_fulldata_normalized.csv",
+              "features_fulldata_normalized_noisegate.csv"]
--- run_all_models.py
+++ run_all_models.py
@@ -2,19 +2,20 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import Perceptron 
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.cluster import AgglomerativeClustering, KMeans
 from sklearn.model_selection import train_test_split 
-from sklearn.preprocessing import normalize
+import pickle
+import os
 
 
 # Run all the models on the same train_test_split 
 
 def run_all_models():
-
-    file_path = 'features.csv'
-    whale_df = pd.read_csv(file_path)
-    X = whale_df.drop(columns='species')
-    y = whale_df[['species']]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
 
     models = []
     
@@ -35,14 +36,68 @@ def run_all_models():
     models.append(MLP_clf)
 
     # SMV 
+    svm_clf = SVC(kernel='rbf',
+              gamma='auto',
+              shrinking=True)
+    models.append(svm_clf)
 
-    
     # KNN 
+    knn_clf = KNeighborsClassifier(n_neighbors=14, weights="distance", p=1)
+    models.append(knn_clf)
+
     # decision tree 
+    decision_tree_clf = DecisionTreeClassifier(criterion="entropy",
+                                 max_depth=None,
+                                 min_impurity_decrease=0.025,
+                                 class_weight="balanced")
+    models.append(decision_tree_clf)
+
+    # # kmeans 
+    # kmeans_clf = KMeans(n_clusters=3, init='random', n_init=1)
+    # models.append(kmeans_clf)
+    
+    # #HAC 
+    # hac_clf = AgglomerativeClustering(n_clusters=3, linkage='complete')
+    # models.append(hac_clf)
 
     # Ensembles 
     # random forest 
+    random_forest_clf = RandomForestClassifier(n_estimators=100)
+    models.append(random_forest_clf)
+
     # gradient boost 
+    gradient_boost_clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3)
+    models.append(gradient_boost_clf)
 
-    
+    data = []
+    for filenames in os.listdir('Datasets'):
+        name = 'Datasets/' + filenames
+        whale_df = pd.read_csv(name)
+        X = whale_df.drop(columns='species')
+        y = whale_df[['species']]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, train_size=0.9)
+        
+        model_names = ['Perceptron', 'MLP', 'SVM', 'KNN', 'Decision Tree', 'Random Forest', 'Gradient Boost']
+        for i in range(len(models)): 
+            model = models[i]
+            model.fit(X_train, y_train)
 
+            model_filename = 'Models/' + model_names[i] + filenames.split('.')[0] + '.pkl'
+            with open(model_filename, 'wb+') as model_file:
+                s = pickle.dump(model, model_file)
+                #pickle.loads(s)
+
+            # get our train and test accuracy write to a csv file
+            train_acc = model.score(X_train, y_train)
+            test_acc = model.score(X_test, y_test)
+
+            # model, dataset, train_acc, test_acc
+            row = [model_names[i], filenames.split('.')[0], train_acc, test_acc]
+            data.append(row)
+
+    data_df = pd.DataFrame(data, columns=['Model', 'Dataset', 'Training Accuracy', 'Test Accuracy'])
+    data_df.to_csv("final_results.csv", index=False)
+
+
+
+run_all_models()
